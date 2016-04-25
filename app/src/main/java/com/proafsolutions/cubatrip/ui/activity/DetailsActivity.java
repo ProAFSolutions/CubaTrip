dc8bcc09d1e1cac024071c81bc8bb81a3b889538
filyjunior <filyjunior@gmail.com>
2016-04-25 15:42:48 -0400

    Update Reviews

--- app/src/main/java/com/proafsolutions/cubatrip/ui/activity/DetailsActivity.java
+++ app/src/main/java/com/proafsolutions/cubatrip/ui/activity/DetailsActivity.java
@@ -4,6 +4,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
 import android.view.View;
 import android.widget.Button;
 
@@ -30,6 +31,18 @@ public class DetailsActivity extends AppCompatActivity {
                 presenter.SetLocation();
             }
         });
+
+        AppCompatButton post = (AppCompatButton) findViewById(R.id.buttonPost);
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                // .setAction("Action", null).show();
+                presenter.NewReview();
+            }
+        });
+
+
     }
 
 
