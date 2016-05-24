de2197d60a9680886e96f5a1429741689326943f
filyjunior <filyjunior@gmail.com>
2016-05-24 16:35:22 -0400

    Add new categories

--- app/src/main/java/com/proafsolutions/cubatrip/ui/util/Utils.java
+++ app/src/main/java/com/proafsolutions/cubatrip/ui/util/Utils.java
@@ -26,6 +26,62 @@ public class Utils {
             {
                 return CategoryEnum.EDUCATION;
             }
+
+            case R.id.imgNightlife:
+            {
+                return CategoryEnum.NIGHTLIFE;
+            }
+
+            case R.id.imgShopping:
+            {
+                return CategoryEnum.SHOPPING;
+            }
+
+            case R.id.imgSport:
+            {
+                return CategoryEnum.SPORT;
+            }
+            case R.id.imgArt:
+            {
+                return CategoryEnum.ART;
+            }
+            case R.id.imgAutomotive:
+            {
+                return CategoryEnum.AUTOMOTIVE;
+            }
+            case R.id.imgBeauty:
+            {
+                return CategoryEnum.BEAUTY;
+            }
+
+            case R.id.imgEvents:
+            {
+                return CategoryEnum.EVENTS;
+            }
+
+            case R.id.imgMedical:
+            {
+                return CategoryEnum.MEDICAL;
+            }
+
+            case R.id.imgServices:
+            {
+                return CategoryEnum.SERVICES;
+            }
+            case R.id.imgPets:
+            {
+                return CategoryEnum.PETS;
+            }
+
+            case R.id.imgGas:
+            {
+                return CategoryEnum.GAS;
+            }
+
+            case R.id.imgWifi:
+            {
+                return CategoryEnum.WIFI;
+            }
         }
 
         return CategoryEnum.RESTAURANTS;
